f4c12191ae4e2106de1c50c6cce0c3b4e05aab37
batmanwayne744-cell <batmanwayne744@gmail.com>
2026-08-11 04:54:48 +0000

    committed

---
+++ program10.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+int main() {
+    int n, i = 1;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    while (i <= n) {
+        printf("%d ", i);
+        i++;
+    }
+
+    return 0;
+}
+
